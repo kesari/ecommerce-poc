0336d5dac2f4b7ee3c0a69b960330cde42244b59
rajmohan <rajmohan141089@gmail.com>
2026-08-25 11:40:28 +0530

    fix: stabilize account service build

--- src/main/java/com/poc/account/infrastructure/mapping/AddressDtoMapper.java
+++ src/main/java/com/poc/account/infrastructure/mapping/AddressDtoMapper.java
@@ -2,10 +2,17 @@ package com.poc.account.infrastructure.mapping;
 
 import com.poc.account.api.dto.AddressResponse;
 import com.poc.account.domain.model.Address;
-import org.mapstruct.Mapper;
+import org.springframework.stereotype.Component;
 
-@Mapper(componentModel = "spring")
-public interface AddressDtoMapper {
+@Component
+public class AddressDtoMapper {
 
-    AddressResponse toResponse(Address address);
+    public AddressResponse toResponse(Address address) {
+        if (address == null) {
+            return null;
+        }
+        return new AddressResponse(address.id(), address.fullName(), address.line1(),
+                address.line2(), address.city(), address.state(), address.postalCode(),
+                address.country(), address.phoneNumber(), address.createdAt(), address.updatedAt());
+    }
 }
